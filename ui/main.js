2098b708af987347b8e4ed20f2af288704715dde
Abhishek kumar verma <abhishekverma2535@gmail.com>
2017-02-15 19:23:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,5 @@
 console.log('Loaded!');
 
-var element=document.getElemenetById('main-text');
-element.innerHTML="new value";
+var element=document.getElementById('main-text');
+
+element.innerHTML='new value';
